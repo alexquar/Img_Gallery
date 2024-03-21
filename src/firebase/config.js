0e3337c61ded1c1526ccbd4a8eed77f93de2a0d8
alexquar <alexanderquarrie@gmail.com>
2024-03-21 14:13:01 -0400

    firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+
+import firebase from 'firebase/app'
+import 'firebase/storage'
+import 'firebase/firestore'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDqRn-bASuhn8SE5_JpTU0FjoPi1jhxqaI",
+    authDomain: "img-u-social.firebaseapp.com",
+    projectId: "img-u-social",
+    storageBucket: "img-u-social.appspot.com",
+    messagingSenderId: "89787067327",
+    appId: "1:89787067327:web:039e1b31f9c6e3882ccf0a",
+    measurementId: "G-275RSH9DNJ"
+  };
+
+
+firebase.initializeApp(firebaseConfig)
+
+const projectStorage = firebase.storage()
+const projectFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectStorage, projectFirestore, timestamp }
