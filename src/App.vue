d9b55ef21a18a7ff8871e154c3ebce94c319c28c
alexquar <alexanderquarrie@gmail.com>
2024-03-21 15:58:19 -0400

    file upload button :(((((((((((((((((((

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="App">
     <HeaderTitle />
+    <UploadForm />
     <ImageGrid />
+
   </div>
 </template>
 
 <script>
 import ImageGrid from './components/ImageGrid.vue'
 import HeaderTitle from './components/HeaderTitle.vue'
-
+import UploadForm from './components/UploadForm.vue'
 export default {
   name: 'App',
   components: {
     ImageGrid,
-    HeaderTitle
+    HeaderTitle,
+    UploadForm
   }
 }
 </script>
