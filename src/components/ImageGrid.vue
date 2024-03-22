dbe6a17e84943ae331e8dd5b717416e8e6616e54
alexquar <alexanderquarrie@gmail.com>
2024-03-22 10:01:13 -0400

    image grid

--- src/components/ImageGrid.vue
+++ src/components/ImageGrid.vue
@@ -1,21 +1,46 @@
 <template>
-  <div>image grid goes here</div>
-    
+  <div class="img-grid">
+
+    <div v-for="img in images" :key="img.id" class="img-wrap">
+      <img :src="img.url" alt="Post">
+    </div>
+
+  </div>
   </template>
   
   <script>
   import useCollection from '../composables/useCollection'
   export default {
   setup(){
-    const {documents, error} = useCollection('images')
+    const {documents: images, error} = useCollection('images')
 
 
 
-    return {documents, error}
+    return {images, error}
   }
   }
   </script>
   
-  <style>
-  
+  <style scoped>
+   .img-grid{
+    margin: 20px auto;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 40px;
+  }
+  .img-wrap{
+    overflow: hidden;
+    height: 0;
+    padding: 50% 0;
+    position: relative;
+    opacity: 0.8;
+  }
+  .img-wrap img{
+    min-width: 100%;
+    min-height: 100%;
+    max-width: 180%;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
   </style>
--- src/composables/useCollection.js
+++ src/composables/useCollection.js
@@ -1,29 +1,36 @@
-import { ref, watchEffect } from "vue"
-import { projectFirestore } from "@/firebase/config"
+import { ref, watchEffect } from 'vue'
+import { projectFirestore } from '../firebase/config'
+
 const useCollection = (collection) => {
-    const documents = ref(null)
-    const error = ref(null)
+  const documents = ref(null)
+  const error = ref(null)
 
-    const collectionRef = projectFirestore.collection(collection).orderBy('createdAt', "desc")
-    const unsub = collectionRef.onSnapshot(()=> {
-        let results = []
-        snaps.docs.forEach(doc=> {
-            results.push({...doc.data, id: doc.id})
-        })
-        documents.value = results
-        error.value = null
-    }, (err) =>{
-        console.log(err.message)
-        error.value = 'could not fetch data'
-        documents.value = null
+  // register the firestore collection reference
+  let collectionRef = projectFirestore.collection(collection)
+    .orderBy('createdAt', 'desc')
+
+  const unsub = collectionRef.onSnapshot(snap => {
+    let results = []
+    snap.docs.forEach(doc => {
+      results.push({...doc.data(), id: doc.id})
     })
+    
+    // update values
+    documents.value = results
+    error.value = null
+    console.log(documents.value)
+  }, err => {
+    console.log(err.message)
+    documents.value = null
+    error.value = 'could not fetch the data'
+  })
 
+  watchEffect((onInvalidate) => {
+    onInvalidate(() => unsub());
+  })
 
-watchEffect( (onInvalidate) => {
-    onInvalidate(()=>unsub())
-})
+  return { error, documents }
 
-    return {documents, error}
 }
 
-export default useCollection
+export default useCollection;
