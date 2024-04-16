d77114bdf9885f62ae3dc6a9de96334dbfa9313e
alexquar <alexanderquarrie@gmail.com>
2024-04-16 08:47:24 -0400

    additions

--- src/components/HeaderTitle.vue
+++ src/components/HeaderTitle.vue
@@ -1,19 +1,37 @@
 <template>
     <div class="title">
-      <h1>img-u</h1>
+      <h1><router-link :to="{ name: 'HomeApp' }">img-u</router-link></h1>
       <p>Your easy to use image sharing platform!</p>
       <div>
-        <div>
+      <div>
       <p> Hey </p>
-      <button class="btn"> </button>
+      <button class="btn" @click="handleSubmit"> Logout </button>
     </div>
     <div>
-      <button class="btn"> </button>
-      <button class="btn"> </button>
+      <button class="btn"> <router-link :to="{ name: 'LoginApp' }">Login</router-link></button>
+      <button class="btn">  <router-link :to="{ name: 'SignupApp' }"> Signup</router-link> </button>
     </div>
     </div>
     </div>
   </template>
+
+  <script>
+  import {useRouter} from 'vue-router'
+  import getUser from '../composables/getUser'
+  import useLogout from '../composables/useLogout'
+  export default {
+    setup(){
+      const router = useRouter()
+      const { logout } = useLogout()
+       const handleSubmit = async () => {
+        await logout()
+        router.push({name:'Login'})
+        }
+        const {user} = getUser()
+        return { handleSubmit, user}
+    }
+  }
+  </script>
   
   <style>
     .title h1{
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeApp from '../views/HomeApp.vue'
+import LoginApp from '../views/LoginApp.vue'
+import SignupApp from '../views/SignupApp.vue'
 const routes = [
   {
     path: '/',
     name: 'HomeApp',
     component: HomeApp,
+  },
+  {
+    path: '/login',
+    name: 'LoginApp',
+    component: LoginApp,
+  },
+  {
+    path: '/signup',
+    name: 'SignupApp',
+    component: SignupApp,
   },
 ]
 
