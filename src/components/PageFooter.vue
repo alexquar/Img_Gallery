f808e0776013d78ba9c7bb66cb27203fb62e137b
alexquar <alexanderquarrie@gmail.com>
2024-04-17 11:06:58 -0400

    conditionally showing footer

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="container">
+    <div class="container footer">
+     <div v-if="user">
         <button class="btn-outline">
             <router-link :to="{ name: 'AboutApp' }"> About</router-link>
         </button>
@@ -10,19 +11,22 @@
             <router-link :to="{ name: 'HomeApp' }"> Home</router-link>
         </button>
     </div>
+    </div>
 </template>
 
 <script>
-
+import getUser from "../composables/getUser"
 export default {
     setup () {
-        
+        const {user} = getUser()
 
-        return {}
+        return {user}
     }
 }
 </script>
 
 <style  scoped>
-
+.footer{
+    margin-top: 20px;
+}
 </style>
