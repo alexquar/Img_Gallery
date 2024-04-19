4787265f5b0b52517383b2e87fb23304ea176405
alexquar <alexanderquarrie@gmail.com>
2024-04-19 11:55:27 -0400

    about page

--- src/views/AboutApp.vue
+++ src/views/AboutApp.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
-
+    <div class="container-col">
+        <h1 class="title">About the App</h1>
+        <p class="body">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus fugiat in sint harum blanditiis nobis eos repellat, ipsum velit perspiciatis modi laboriosam est placeat tempore quam autem recusandae consectetur, aspernatur, cumque nostrum. Quae molestiae reprehenderit unde, aspernatur accusamus atque itaque veritatis obcaecati, hic libero iusto aut nemo ullam dolorum esse soluta expedita quod, nulla sit mollitia cupiditate! Eius, ipsam? Ipsa, repellendus nam provident vero officiis et? Rem atque sit ducimus tempore, totam eveniet, autem commodi sapiente nihil aliquid minima similique at facere sunt nostrum sed, dolorum amet saepe! Inventore officiis maiores dignissimos delectus mollitia aspernatur molestiae vitae commodi, tempore atque placeat aut quos corporis repudiandae fuga vel non sunt. Harum non, tempore explicabo eius cum, libero voluptatibus sequi assumenda est quaerat vitae eveniet voluptas, illum repellendus. Alias consectetur amet officia fugit est eum non voluptas repellendus a aspernatur voluptatibus consequatur repudiandae, quibusdam voluptate temporibus eius odit numquam libero recusandae! Harum non fugiat deserunt. Impedit eaque perspiciatis tempore molestiae velit maiores officia aperiam. Numquam necessitatibus iste vitae aliquid nam nisi possimus, incidunt animi, dolore, fuga ex a itaque reiciendis dolores. Architecto doloremque natus harum corrupti iste repudiandae voluptate perspiciatis nobis sit assumenda voluptatem beatae tenetur pariatur, quaerat aliquam quod alias adipisci.
+        </p>
+        <p class="body">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus fugiat in sint harum blanditiis nobis eos repellat, ipsum velit perspiciatis modi laboriosam est placeat tempore quam autem recusandae consectetur, aspernatur, cumque nostrum. Quae molestiae reprehenderit unde, aspernatur accusamus atque itaque veritatis obcaecati, hic libero iusto aut nemo ullam dolorum esse soluta expedita quod, nulla sit mollitia cupiditate! Eius, ipsam? Ipsa, repellendus nam provident vero officiis et? Rem atque sit ducimus tempore, totam eveniet, autem commodi sapiente nihil aliquid minima similique at facere sunt nostrum sed, dolorum amet saepe! Inventore officiis maiores dignissimos delectus mollitia aspernatur molestiae vitae commodi, tempore atque placeat aut quos corporis repudiandae fuga vel non sunt. Harum non, tempore explicabo eius cum, libero voluptatibus sequi assumenda est quaerat vitae eveniet voluptas, illum repellendus. Alias consectetur amet officia fugit est eum non voluptas repellendus a aspernatur voluptatibus consequatur repudiandae, quibusdam voluptate temporibus eius odit numquam libero recusandae! Harum non fugiat deserunt. Impedit eaque perspiciatis tempore molestiae velit maiores officia aperiam. Numquam necessitatibus iste vitae aliquid nam nisi possimus, incidunt animi, dolore, fuga ex a itaque reiciendis dolores. Architecto doloremque natus harum corrupti iste repudiandae voluptate perspiciatis nobis sit assumenda voluptatem beatae tenetur pariatur, quaerat aliquam quod alias adipisci.
+        </p>
     </div>
 </template>
 
@@ -14,6 +18,9 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style  scoped>
+.body{
+    color: #aaa;
+    margin: 20px;
+}
 </style>
